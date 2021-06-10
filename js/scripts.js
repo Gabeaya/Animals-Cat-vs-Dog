ab2c78f0a43d543aeae9f51e04e504eca19eb862
Gabriel Ayala <gabeayala100@gmail.com>
2021-06-09 21:24:24 -0700

    call the id created in html and provide a response for the nosie column.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
   $("button#cat").click(function() {
-    $("ul").prepend("<li>Cat</li>");
+    $("ul#animal").prepend("<li>Cat</li>");
+    $("ul#noise").prepend("<li>MEOOOOOW</li>");
   });
 
   $("button#dog").click(function() {
-    $("ul").prepend("<li>Dog</li>");
+    $("ul#animal").prepend("<li>Dog</li>");
+    $("ul#noise").prepend("<li>barrrarroof</li>");
   });
 
   $("button#fox").click(function() {
-    $("ul").prepend("<li>Fox</li>");
+    $("ul#animal").prepend("<li>Fox</li>");
+    $("ul#noise").prepend("<li>Hahtehahtehatehah</li>");
   });
 
   $("button#pig").click(function() {
-    $("ul").prepend("<li>Pig</li>");
+    $("ul#animal").prepend("<li>Pig</li>");
+    $("ul#noise").prepend("<li>Weeweewewewi</li>");
   });
 })
